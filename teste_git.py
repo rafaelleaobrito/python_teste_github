a01993f9a4fdba968645d656acb6c3f9f784d327
rafaelleaobrito <rafaelleaobrito@gmail.com>
2023-05-08 16:54:44 -0300

    acrescentado o valor do imposto

--- teste_git.py
+++ teste_git.py
@@ -4,9 +4,10 @@ def input_float(texto):
     return float(valor)
 
 
-def calcular_lucro(valor_custo, taxa_categoria, valor_final):
+def calcular_lucro(valor_custo, taxa_categoria, valor_final, imposto):
     taxa_valor = valor_final * (taxa_categoria / 100)
-    custo_total = valor_custo + taxa_valor + 5
+    valor_imposto = valor_final * (imposto / 100)
+    custo_total = valor_custo + taxa_valor + 5 + valor_imposto
     lucro = valor_final - custo_total
     porcentagem_lucro = (lucro / valor_final) * 100
 
@@ -15,10 +16,11 @@ def calcular_lucro(valor_custo, taxa_categoria, valor_final):
 
 valor_custo = input_float("Digite o valor de custo do produto: ")
 taxa_categoria = input_float("Digite a taxa da categoria (em porcentagem): ")
+imposto = input_float("Digite o imposto a ser pago(em porcentagem): ")
 valor_final = input_float("Digite o valor final de venda do produto: ")
 
 lucro, porcentagem_lucro = calcular_lucro(
-    valor_custo, taxa_categoria, valor_final)
+    valor_custo, taxa_categoria, valor_final, imposto)
 
 print(f"\nLucro: R$ {lucro:.2f}")
 print(f"Porcentagem de lucro: {porcentagem_lucro:.2f}%")
